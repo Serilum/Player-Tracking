b0c85e2f070d9b3b9769614ad9bc8d406bfc00aa
Rick South <rick@natamus.com>
2023-12-12 00:30:40 +0100

    Player Tracking source code for 1.20.3.

---
+++ Fabric/src/main/java/com/natamus/playertracking/ModFabric.java
@@ -0,0 +1,30 @@
+package com.natamus.playertracking;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.playertracking.cmds.CommandTrack;
+import com.natamus.playertracking.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			CommandTrack.register(dispatcher);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
