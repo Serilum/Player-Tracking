f3f3b2043f814740563897ee1de9c1e087ba5c02
Rick South <rick@natamus.com>
2024-05-01 16:33:44 +0200

    Player Tracking source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/playertracking/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.playertracking.util;
+
+public class Reference {
+	public static final String MOD_ID = "playertracking";
+	public static final String NAME = "Player Tracking";
+	public static final String VERSION = "3.3";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
