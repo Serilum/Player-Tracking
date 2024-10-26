ae108e91f7a79dcd43630609ecd2dac1da166b84
Rick South <rick@natamus.com>
2024-10-26 15:04:48 +0200

    Player Tracking source code for 1.21.3.

---
+++ NeoForge/src/main/java/com/natamus/playertracking/neoforge/events/NeoForgeCommandRegisterEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.playertracking.neoforge.events;
+
+import com.natamus.playertracking.cmds.CommandTrack;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeCommandRegisterEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandTrack.register(e.getDispatcher());
+	}
+}
