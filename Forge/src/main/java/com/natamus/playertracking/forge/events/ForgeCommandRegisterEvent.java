758edfc2bcb23f9648c9bf5b38faffb1feb2bba4
Rick South <rick@natamus.com>
2025-02-12 23:20:31 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/playertracking/forge/events/ForgeCommandRegisterEvent.java
+++ Forge/src/main/java/com/natamus/playertracking/forge/events/ForgeCommandRegisterEvent.java
@@ -3,12 +3,10 @@ package com.natamus.playertracking.forge.events;
 import com.natamus.playertracking.cmds.CommandTrack;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeCommandRegisterEvent {
     @SubscribeEvent
-    public void registerCommands(RegisterCommandsEvent e) {
+    public static void registerCommands(RegisterCommandsEvent e) {
     	CommandTrack.register(e.getDispatcher());
     }
 }
--- NeoForge/src/main/java/com/natamus/playertracking/neoforge/events/NeoForgeCommandRegisterEvent.java
+++ NeoForge/src/main/java/com/natamus/playertracking/neoforge/events/NeoForgeCommandRegisterEvent.java
@@ -3,9 +3,7 @@ package com.natamus.playertracking.neoforge.events;
 import com.natamus.playertracking.cmds.CommandTrack;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeCommandRegisterEvent {
 	@SubscribeEvent
 	public static void registerCommands(RegisterCommandsEvent e) {
